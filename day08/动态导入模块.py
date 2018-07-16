6d906ae20b5593e7cc68895a3d60501db7255ca1
qingDragon <921971961@qq.com>
2018-07-17 01:03:25 +0800

    mac7.17

--- day08/动态导入模块.py
+++ day08/动态导入模块.py
@@ -10,7 +10,5 @@
 #官方推荐方法
 
 import importlib
-
 aa = importlib.import_module("lib.aa")
-
 print(aa.C().name)
--- day4/生成器并行.py
+++ day4/生成器并行.py
@@ -1,30 +1,27 @@
-__author__ = "Alex Li"
-
 import time
+
+
 def consumer(name):
-    print("%s 准备吃包子啦!" %name)
+    print("%s 准备吃包子啦!" % name)
     while True:
-       baozi = yield
+        baozi = yield
+        print("包子[%s]来了，被[%s]吃了" % (baozi, name))
 
-       print("包子[%s]来了,被[%s]吃了!" %(baozi,name))
-
-c = consumer("ChenRonghua")
-c.__next__()
-
-# b1= "韭菜馅"
-# c.send(b1)
+# c = consumer("LvSixian")
 # c.__next__()
+# b1 = "韭菜馅"
+# c.send(b1)
 
 def producer(name):
-    c = consumer('A')
-    c2 = consumer('B')
+    c = consumer("A")
+    c2 = consumer("B")
     c.__next__()
     c2.__next__()
-    print("老子开始准备做包子啦!")
+    print("老子开始做包子啦")
     for i in range(10):
         time.sleep(1)
-        print("做了1个包子,分两半!")
+        print("做了1个包子，分两半!")
         c.send(i)
         c2.send(i)
 
-producer("alex")
+producer("LvSixian")
